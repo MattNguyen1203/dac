196a5ad82f858ebe84faed4aa96cae8872a7c7ab
Matt <huongax123@gmail.com>
2024-03-18 15:28:57 +0700

    update

--- build.sh
+++ build.sh
@@ -1,13 +1,13 @@
-#!/bin/bash
+# #!/bin/bash
 
-# Create a network, which allows containers to communicate
-docker network create dacnetwork
+# # Create a network, which allows containers to communicate
+# docker network create dacnetwork
 
-# Build production
-docker compose -f docker-compose.yml build
+# # Build production
+# docker compose -f docker-compose.yml build
 
-# Up dev
-docker compose -f docker-compose.yml up -d
+# # Up dev
+# docker compose -f docker-compose.yml up -d
 
-# Reload nginx
-sudo systemctl reload nginx
+# # Reload nginx
+# sudo systemctl reload nginx
